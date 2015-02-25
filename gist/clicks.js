a7569e8ca0efe61f04e8aaca1f6a2c3bf9107f8a
Juan sebastian <sebastirojo@yahoo.com>
2015-02-24 16:39:26 -0800

    hgfcd

--- gist/clicks.js
+++ gist/clicks.js
@@ -1,3 +1,6 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
 $("h1.page-title").text("My code runs!");
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.pageX});
+});
