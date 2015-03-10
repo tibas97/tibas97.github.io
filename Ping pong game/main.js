1e4770a794ebb572af102e95ef77bf195e5b6826
Juan sebastian <sebastirojo@yahoo.com>
2015-03-10 16:50:08 -0700

    hv

--- Ping pong game/main.js
+++ Ping pong game/main.js
@@ -1,26 +1,4 @@
 
-var main = {
-  preload: function() {
-    // This function will be executed at the beginning     
-        // That's where we load the game's assets  
-  },
-
-  create: function() { 
-    // This function is called after the preload function     
-        // Here we set up the game, display sprites, etc. 
-  },
-
-  update: function() {
-    // This function is called 60 times per second    
-        // It contains the game's logic     
-  },
-};
-
-// Initialize Phaser, and start our 'main' state 
-var game = new Phaser.Game(400, 450, Phaser.AUTO, 'gameDiv');
-game.state.add('main', main);
-game.state.start('main');
-
 preload: function() {
   // Load the paddle image
   game.load.image('paddle', 'assets/paddle.png');
@@ -56,3 +34,4 @@ update: function() {
   else 
     this.paddle.body.velocity.x = 0;  
 }
+
