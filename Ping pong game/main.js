13de38b0736d716e8e35044a001b6b6dc0a5aa00
Juan sebastian <sebastirojo@yahoo.com>
2015-03-10 17:21:39 -0700

    sxsd

--- Ping pong game/main.js
+++ Ping pong game/main.js
@@ -1,9 +1,13 @@
-
-preload: function() {
+// Define our main state
+var main = {
+  preload: function() {
   // Load the paddle image
   game.load.image('paddle', 'assets/paddle.png');
-},
-
+// Load the brick sprite
+game.load.image('brick', 'assets/brick.png');
+// Load the ball sprite
+game.load.image('ball', 'assets/ball.png');
+  },
 create: function() { 
   // Initialize the physics system of the game
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -19,6 +23,29 @@ create: function() {
 
   // Make sure the paddle won't move when hit by the ball
   this.paddle.body.immovable = true;
+// Create a group that will contain all the bricks
+this.bricks = game.add.group();
+this.bricks.enableBody = true;
+
+// Create the 16 bricks
+for (var i = 0; i < 5; i++)
+  for (var j = 0; j < 5; j++)
+    game.add.sprite(55+i*60, 55+j*35, 'brick', 0, this.bricks);
+
+// Make sure that the bricks won't move
+this.bricks.setAll('body.immovable', true);
+// Create the ball with physics
+this.ball = game.add.sprite(200, 300, 'ball');
+game.physics.arcade.enable(this.ball);
+
+// Add velocity to the ball
+this.ball.body.velocity.x = 1000; 
+this.ball.body.velocity.y = 1000;
+
+// Make the ball bouncy 
+this.ball.body.collideWorldBounds = true;
+this.ball.body.bounce.x = 1; 
+this.ball.body.bounce.y = 1;
 },
 
 update: function() {
@@ -33,5 +60,20 @@ update: function() {
   // If no arrow is pressed, stop moving
   else 
     this.paddle.body.velocity.x = 0;  
+// Make the paddle and the ball collide
+game.physics.arcade.collide(this.paddle, this.ball);
+
+// Call the 'hit' function when the ball hit a brick
+game.physics.arcade.collide(this.ball, this.bricks, this.hit, null, this);
+},
+hit: function(ball, brick) {
+  // When the ball hits a brick, kill the brick
+  brick.kill();
 }
+};
+    
+// Initialize Phaser, and start our 'main' state 
+var game = new Phaser.Game(400, 450, Phaser.AUTO, 'gameDiv');
+game.state.add('main', main);
+game.state.start('main');
 
